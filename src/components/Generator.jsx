a4573797b29046cebc158b48afd4cc17ef973a04
Alexander Bryant <asbryant@me.com>
2025-02-06 18:40:05 -0700

    Adding button colour change
    Colour change when buttons selected in section one.

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SectionWrapper from './SectionWrapper'
 import { SCHEMES, WORKOUTS } from '../utils/exercises.js'
 
@@ -25,6 +25,10 @@ function toggleModal() {
   setShowModal(!showModal)
 }
 
+useEffect(() => {
+  console.log(`current type is: ${poison}`)
+}, [poison])
+
   return (
     <section>
       <SectionWrapper
@@ -41,7 +45,12 @@ function toggleModal() {
         />
         <div className='grid grid-cols-2 sm:grid-cols-4 gap-4'>
           {Object.keys(WORKOUTS).map((type, typeIndex) => (
-            <button className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg' key={typeIndex}>
+            <button 
+              onClick={() => {
+                setPoison(type)
+              }}
+              className={'bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg cursor-pointer' + (type === poison ? ' border-blue-600' : ' border-blue-400')} 
+              key={typeIndex}>
               <p className='capitalize'>{type.replaceAll('_', " ")}</p>
             </button>
           ))}
@@ -54,7 +63,7 @@ function toggleModal() {
           title={'Choose your targets'}
           description={'Select the muscles to go to work on.'}
         />
-        <div className='bg-slate-950 border border-solid border-blue-400 rounded-lg flex flex-col'>
+        <div className='bg-slate-950 border border-solid border-blue-400 hover:border-blue-600 rounded-lg flex flex-col'>
           <button onClick={toggleModal} className='relative p-3 flex items-center justify-center cursor-pointer'>
             <p>Select muscle groups</p>
             <i className="fa-solid absolute right-3 top-1/2 -translate-y-1/2 fa-caret-down"></i>
@@ -68,12 +77,12 @@ function toggleModal() {
 
         <Header
           index={'03'}
-          title={'Pick your poison'}
-          description={'Select the workout you wish to endure.'}
+          title={"Decided how you'll grow"}
+          description={'Select your ultimate objective.'}
         />
         <div className='grid grid-cols-3 gap-4'>
           {Object.keys(SCHEMES).map((scheme, schemeIndex) => (
-            <button className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg' key={schemeIndex}>
+            <button className='bg-slate-950 border border-blue-400 duration-200 hover:border-blue-600 py-3 rounded-lg cursor-pointer' key={schemeIndex}>
               <p className='capitalize'>{scheme.replaceAll('_', " ")}</p>
             </button>
           ))}
